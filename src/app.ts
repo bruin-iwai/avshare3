8e4f9bc6f56df0fb6e67589ecc46c7bea5b73748
Katsuhito Iwai <katsuhito.iwai@toshiba.co.jp>
2023-09-17 06:24:39 +0000

    basic認証組み込み

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import compression from 'compression';
+import { default as compare } from 'tsscmp';
+import auth from 'basic-auth';
 import { apiRouter } from '~/api';
 
 const app = express();
@@ -10,6 +12,23 @@ const port = parseInt(process.env.PORT || '80', 10);
 app.use(helmet());
 app.use(cors());
 app.use(compression());
+
+// basic auth
+app.use((req, res, next) => {
+  const check = (name: string, pass: string): boolean =>
+    Boolean(name) &&
+    Boolean(pass) &&
+    compare(name, process.env.USERNAME || '') &&
+    compare(pass, process.env.PASSWORD || '');
+  const credentials = auth(req);
+
+  if (!credentials || !check(credentials.name, credentials.pass)) {
+    res.setHeader('WWW-Authenticate', 'Basic realm="avshare3"');
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world');
