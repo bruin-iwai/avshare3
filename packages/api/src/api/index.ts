bb968e5bd69d59c68c7f726228851acfecfa5239
Katsuhito Iwai <bruin.iwai@gmail.com>
2025-03-02 16:19:30 +0900

    「GET /」を /api の下に

--- packages/api/src/api/index.ts
+++ packages/api/src/api/index.ts
@@ -3,6 +3,27 @@ import { contentsListHandler } from './contentsListHandler';
 
 export const apiRouter: FastifyPluginAsync = async (fastify, _opts) => {
   fastify.get(
+    '/',
+    {
+      schema: {
+        summary: 'Hello world',
+        tags: ['general'],
+        response: {
+          '200': {
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    async (_req, _reply) => {
+      return { message: 'Hello world' };
+    },
+  );
+
+  fastify.get(
     '/contentsList',
     {
       schema: {
