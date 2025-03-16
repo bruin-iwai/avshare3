31a591c6fdf029d9a341af9d0ef9bede4b9831a9
Katsuhito Iwai <bruin.iwai@gmail.com>
2025-03-16 23:08:42 +0900

    Nuxt の layouts / pages 構造を使う

--- packages/gui/app.vue
+++ packages/gui/app.vue
@@ -1,28 +1,5 @@
 <template>
-  <div>
-    <v-app>
-      <v-app-bar>
-        <v-app-bar-title>My favorite videos</v-app-bar-title>
-        <template #append>
-          <v-checkbox v-model="isDark" :label="themeLabel" />
-        </template>
-      </v-app-bar>
-      <v-main>
-        <MainContent />
-      </v-main>
-    </v-app>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<script setup lang="ts">
-import { useTheme } from 'vuetify';
-import { useDarkTheme } from './composables/useMyTheme';
-
-const theme = useTheme();
-const isDark = useDarkTheme();
-const themeLabel = computed(() => (isDark.value ? 'dark mode' : 'light mode'));
-
-watchEffect(() => {
-  theme.global.name.value = isDark.value ? 'dark' : 'light';
-});
-</script>
