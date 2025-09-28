fb115bf78eb4ab7ee4d8c572d1b32c20d63c5461
Katsuhito Iwai <bruin.iwai@gmail.com>
2025-09-28 18:15:46 +0900

    ADOT Lambda layer 導入

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,9 +6,11 @@ import { PromiseResolver } from '@h4ad/serverless-adapter/resolvers/promise';
 import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/adapters/aws';
 import { createApp } from './app';
 
-export const handler = ServerlessAdapter.new(null)
+const handler = ServerlessAdapter.new(null)
   .setFramework(new LazyFramework(new FastifyFramework(), createApp))
   .setHandler(new DefaultHandler())
   .setResolver(new PromiseResolver())
   .addAdapter(new ApiGatewayV2Adapter())
   .build();
+
+module.exports = { handler };
