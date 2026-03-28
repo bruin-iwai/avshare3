d91307e1a6788f438919bdb11d9af8c8bce494e2
Katsuhito Iwai <bruin.iwai@gmail.com>
2026-03-28 16:14:56 +0900

    fix build errors

--- packages/api/build.mjs
+++ packages/api/build.mjs
@@ -3,8 +3,9 @@ import * as esbuild from 'esbuild';
 await esbuild.build({
   entryPoints: ['src/standalone.ts'],
   bundle: true,
-  outfile: 'dist/index.js',
-  target: 'es2020',
+  outfile: 'dist/index.mjs',
+  target: 'node24',
+  format: 'esm',
   platform: 'node',
-  external: ['@aws-sdk', '@fastify/swagger', '@fastify/swagger-ui'],
+  external: ['@aws-sdk/*', '@fastify/*', 'fastify', 'http-errors'],
 });
